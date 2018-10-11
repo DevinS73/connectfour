1ad7806742200ef4aa3fd9f743037148f1b9aea1
Devin Schwartz <devinschwartz9@gmail.com>
2018-10-11 13:50:17 -0500

    initial commit

---
+++ player.py
@@ -0,0 +1,8 @@
+class Player:
+    def __init__(self):
+        self.name=self.get_name()
+        self.piece=piece
+    def get_name(self):
+        pass
+    def get_choice(self,board):
+        pass
