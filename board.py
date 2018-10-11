6ee7b40818acfd769d7b911ae301a29dacc9f373
THREATT.KYLE <2200796@fbisd.flourbluffschools.net>
2018-10-11 13:55:55 -0500

    defined class with methods

--- board.py
+++ board.py
@@ -1,1 +1,36 @@
-#board
+
+class Board():
+    def __init__(self,w,h):
+        self.width = w
+        self.height = h
+    
+    def place_piece(self,piece,column):
+        pass
+    
+    def empty_board(self):
+        pass
+    
+    def check_win(self):
+        pass
+    
+    def is_fool(self):
+        pass
+    
+    def disp_board(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
