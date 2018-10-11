042e10e8fce8c35bf19777b4fe1326ccbb9765a6
THREATT.KYLE <2200796@fbisd.flourbluffschools.net>
2018-10-11 14:12:57 -0500

    finished emptyboard started display

--- board.py
+++ board.py
@@ -3,34 +3,36 @@ class Board():
     def __init__(self,w,h):
         self.width = w
         self.height = h
+        self.board = [[' ']*w for i in range(h)]
     
-    def place_piece(self,piece,column):
-        pass
-    
+#    def place_piece(self,piece,column):
+#        pass
+#        for i in range(self.width):
+#            for ele in row:
+#                if ele == 0
+#        self.board[][column]
+#    
     def empty_board(self):
-        pass
+        self.board = [[' ']*w for i in range(h)]
     
     def check_win(self):
         pass
+        #return bool
     
     def is_fool(self):
         pass
+        #return bool
     
     def disp_board(self):
-        pass
-
-
-
-
-
-
-
-
-
-
-
-
+        for row in self.board:
+            for ele in row:
+                print(ele,end='|')
+            print()
+        
+        for i in range(self.width):
+            print(i+1,end=' ')
 
 
 if __name__ == "__main__":
-    pass
+    b = Board(6,6)
+    b.disp_board()
