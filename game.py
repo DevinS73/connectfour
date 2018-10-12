2cce568543e2dd34f36a6a3fd1b96b986005beb0
Devin Schwartz <devinschwartz9@gmail.com>
2018-10-12 14:12:13 -0500

    worked on play game and changed get choice

--- game.py
+++ game.py
@@ -1,16 +1,34 @@
-#from board import Board
+from board import Board
 from player import Player
 class Game:
    def __init__(self,players,board):
       self.turn=0
-      self.players=player
+      self.players=players
       self.board=board
    def play_game(self):
       print("Welcome to connect four!")
-      
+      turn=0
+      while board.check_win()==False and board.is_full()==False:
+        players[turn%len(players)].get_choice()
+          try:
+              board.place_piece(players[turn%len(players)].piece,players[turn%len(players)].get_choice())
+              board.disp_board()
+              if board.check_win()==True:
+                  print(f'Congratualations, {players[turn%len(players)].name}, you win!')
+                  board.empty_board()
+                  break
+              if board.is_full()==True:
+                  print("The board is full, it's a draw.")
+                  board.empty_board()
+                  break
+              turn+=1
+          except Exception as e:
+              print(f'Error: {e}')
 
 if __name__=="__main__":
-   player1=Player()
-   player2=Player()
-   game=Game([player1,player2],board)
-   pass
+   print("For player 1 (O's):")
+   player1=Player('O')
+   print()
+   print("For player 2 (X's):")
+   player2=Player('X')
+   game=Game([player1,player2],Board(7,6))
--- player.py
+++ player.py
@@ -1,11 +1,11 @@
 class Player:
-    def __init__(self):
+    def __init__(self,piece):
         self.name=self.get_name()
         self.piece=piece
     def get_name(self):
         name=input("Enter the player's name: ")
         return name
     def get_choice(self,board):
-        print("What column would you like to place your piece in?")
+        print(f"Choose a column number between 0 and {board.width}")
         choice=input('> ')
         return choice
