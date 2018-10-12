012c39d254f4f4aaa0fe6c268d7ac3898222e46e
THREATT.KYLE <2200796@fbisd.flourbluffschools.net>
2018-10-12 14:13:25 -0500

    finished most module execept add piece and check win

--- board.py
+++ board.py
@@ -5,28 +5,58 @@ class Board():
         self.height = h
         self.board = [[' ']*w for i in range(h)]
     
-#    def place_piece(self,piece,column):
-#        pass
-#        for i in range(self.width):
-#            for ele in row:
-#                if ele == 0
-#        self.board[][column]
-#    
+    def add_piece(self,column,piece):
+        for i in range(self.height-1, 0, -1):            
+            if self.board[i][column-1] != ' ':
+                self.board[i][column-1] = piece
+                break                
+    
     def empty_board(self):
-        self.board = [[' ']*w for i in range(h)]
+        self.board = [[' ']*self.width for i in range(self.height)]
     
-    def check_win(self):
-        pass
-        #return bool
+#    def check_win(self):
+#        pass
+#        c = 0
+#        for i in range(self.height):
+#            for j in range(self.width):
+#                if self.board[i][j] == 'X':
+#                    c += 1
+#                else:
+#                    c = 0
+#                if c >= 4:
+#                    return True
+#            c = 0
+#            
+#        c = 0
+#        for i in range(self.height):
+#            for j in range(self.width):
+#                if self.board[i][j] == 'O':
+#                    c += 1
+#                else:
+#                    c = 0
+#                if c >= 4:
+#                    return True
+#            c = 0
+#            
+#        c = 0
+#        for i in range(self.width):
+#            for j in range(self.height):
+#                if self.[j][i]
+#                
+#        return False
+#        #return bool
     
-    def is_fool(self):
-        pass
-        #return bool
+    def is_full(self):
+        for row in self.board:
+            for ele in row:
+                if ele == ' ':
+                    return False
+        return True
     
     def disp_board(self):
         for row in self.board:
             for ele in row:
-                print(ele,end='|')
+                print(ele,end=' ')
             print()
         
         for i in range(self.width):
