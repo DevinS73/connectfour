6767ff352c5cc687aa55443d04458c77b41b5c61
THREATT.KYLE <2200796@fbisd.flourbluffschools.net>
2018-10-18 13:46:15 -0500

    fixed bug

--- board.py
+++ board.py
@@ -67,6 +67,7 @@ class Board():
         c = 0
         for i in range(self.height-4):
             for j in range(self.width-4):
+                c = 0
                 for k in range(4):
                     if self.board[i+k][j+k] == 'x':
                         c += 1
@@ -74,11 +75,12 @@ class Board():
                         c = 0
                     if c >= 4:
                         return True
-            c = 0
+            
                 
         c = 0
         for i in range(self.height-3):
             for j in range(self.width-3):
+                c = 0
                 for k in range(4):
                     if self.board[i+k][j+k] == 'o':
                         c += 1
@@ -86,11 +88,12 @@ class Board():
                         c = 0
                     if c >= 4:
                         return True
-            c = 0
+            
                 
         c = 0
         for i in range(self.height-1,self.height-4,-1):
             for j in range(self.width-4):
+                c = 0
                 for k in range(4):
                     if self.board[i-k][j+k] == 'x':
                         c += 1
@@ -98,11 +101,11 @@ class Board():
                         c = 0
                     if c >= 4:
                         return True
-            c = 0
                 
         c = 0
         for i in range(self.height-1,self.height-4,-1):
             for j in range(self.width-4):
+                c = 0
                 for k in range(4):
                     if self.board[i-k][j+k] == 'o':
                         c += 1
@@ -110,7 +113,6 @@ class Board():
                         c = 0
                     if c >= 4:
                         return True
-            c = 0
         
         return False
     
@@ -137,29 +139,18 @@ class Board():
 
 
 if __name__ == "__main__":
-#    b = Board(6,7)
-#    b.add_piece(1,'x')
-#    b.add_piece(2,'o')
-#    b.add_piece(2,'x')
-#    b.add_piece(3,'o')
-#    b.add_piece(3,'o')
-#    b.add_piece(3,'x')
-#    b.add_piece(4,'o')
-#    b.add_piece(4,'o')
-#    b.add_piece(4,'o')
-#    b.add_piece(4,'x')
-#    b.disp_board()    
-#    print(b.check_win())
     new_board = Board(7,6)
     new_board.add_piece(1,'x')
-    new_board.add_piece(1,'x')
-    new_board.add_piece(1,'x')
+    new_board.add_piece(1,'o')
     new_board.add_piece(1,'o')
     new_board.add_piece(2,'x')
+    new_board.add_piece(2,'o')
     new_board.add_piece(2,'x')
     new_board.add_piece(2,'o')
+    new_board.add_piece(3,'o')
     new_board.add_piece(3,'x')
     new_board.add_piece(3,'o')
+    new_board.add_piece(4,'o')
     new_board.add_piece(4,'o')
     new_board.disp_board()
     print(new_board.check_win())
